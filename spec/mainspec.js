8a7cf8e70af9a1b3d5bc73ce04294b8413a8a177
Hack Reactor Student <student@hackreactor.com>
2017-08-18 17:54:12 -0700

    initiated template testing repository

---
+++ spec/mainspec.js
@@ -0,0 +1,9 @@
+describe('Hello world', function () {
+  it('says hello', function () {
+    expect(helloWorld()).toEqual('Hello world!');
+  });
+  it('says something else', function () {
+    expect(helloWorld()).toEqual('Hello world!');
+  });
+});
+
